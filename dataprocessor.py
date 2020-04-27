9099eb5668accbcd091fbb0602074a585b18e0e0
leroy <leroyjvargis@gmail.com>
2020-04-26 23:25:19 -0700

    update to add get100Kdata

--- dataprocessor.py
+++ dataprocessor.py
@@ -46,6 +46,49 @@ def get50kdata():
         fiftyKdata.to_csv('combined_data.csv', mode='a', index=False, header=idx == 0)
         print ("Parsed", category)
 
+def get100kdata():
+    ## get 100K from each category
+    ## filter data to be only from 2017
+
+    directory = '/media/leroy/OS/School/_CSE 578/Project/'
+    files = ['Sports_and_Outdoors_5.json', 'Home_and_Kitchen_5.json', 'Electronics_5.json', 'Clothing_Shoes_and_Jewelry_5.json', 'Books_5.json']
+    size = 50000
+    start_date = "2017-1-1"
+    end_date = "2017-12-31"
+
+    starttime = datetime.now()
+    for idx, f in enumerate(files):
+        filename = directory + f
+        category = f.split(".")[0]
+        fiftyKdata = pd.DataFrame()
+        df = pd.read_json(filename, lines=True, convert_dates=['unixReviewTime'], chunksize=size) #, names=names)
+        for each in df:
+            # filter to include only those from 2017
+            after_start_date = each["unixReviewTime"] >= start_date
+            before_end_date = each["unixReviewTime"] <= end_date
+            between_two_dates = after_start_date & before_end_date
+            df_data = each.loc[between_two_dates]
+            
+            if len(fiftyKdata) == 0:
+                fiftyKdata = df_data
+            else:
+                fiftyKdata = pd.concat([fiftyKdata, df_data], ignore_index=True, names=names)
+                
+                # keep only 100K, sample randomly so data is not skewed
+                if fiftyKdata['asin'].count() > 100000:
+                    fiftyKdata = fiftyKdata.sample(100000)
+            fiftyKdata = add_missing_cols(fiftyKdata)
+            print ("elapsed:", str(int((datetime.now() - starttime).total_seconds() // 60)), "minutes", str(int((datetime.now() - starttime).total_seconds() %60)), "seconds", "found:", fiftyKdata['asin'].count(), end='\r' )
+            
+        fiftyKdata['category'] = category
+        fiftyKdata = fiftyKdata[names + ['category']]
+        savefilename = "filtered_data/" + category.split('_')[0] +'_filtered_data.csv'
+        fiftyKdata.to_csv(savefilename, mode='a', index=False, header=True)
+        print ("Finished parsing:", category)
+        
+        
+
+
 def filterByAsin():
     ## filter meta_data files to include only those products who appear in combined_data
     ## doing it this way cos data is too large to hold in-memory, and since products repeat. 
@@ -102,7 +145,16 @@ def addProductName():
     all_meta = pd.concat([all_meta, electronics_meta_filtered], ignore_index=True, names=names)
     all_meta = pd.concat([all_meta, clothing_meta_filtered], ignore_index=True, names=names)
     all_meta = pd.concat([all_meta, books_meta_filtered], ignore_index=True, names=names)
+    all_meta.drop_duplicates(subset=['asin'], inplace=True)
+    all_meta.reset_index(inplace=True, drop=True)
+
+    # keep only these columns from main data
+    filtered_columns = ['overall', 'reviewTime', 'reviewerID', 'asin', 'reviewerName', 'reviewText', 'summary', 'unixReviewTime', 'category']
 
     # add product name and price to combined_data df
-    combined_data_with_title = pd.merge(combined_data,all_meta[['asin','title', 'price']],on='asin', how='left')
-    combined_data_with_title.to_csv("combined_data_with_title.csv", index=False)
+    # combined_data_with_title = pd.merge(combined_data,all_meta[['asin','title', 'price']],on='asin', how='left')
+    # combined_data_with_title.to_csv("combined_data_with_title.csv", index=False)
+
+    # add product name, price and brand to main_data
+    combined_filtered_data_with_metadata = pd.merge(combined_data[filtered_columns] ,all_meta[['asin','title', 'price', 'brand']],on='asin', how='left')
+    combined_filtered_data_with_metadata.to_csv('filtered_data/combined_filtered_with_metadata.csv', index=False, header=True)
